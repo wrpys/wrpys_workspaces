0c2cb4c7891131e7514d8e229b6bc19fab122788
wrpys <496732709@qq.com>
2017-11-07 19:37:36 +0800

    init

---
+++ eureka-server/src/main/java/com/wrpys/app/es/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.wrpys.app.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
