935f7f6034b0ea21807e30493d322a5d8f503e3a
wrpys <496732709@qq.com>
2017-12-04 13:50:32 +0800

    微信接入

--- weixin/src/main/java/com/wrpys/app/wx/service/impl/CoreServiceImpl.java
+++ weixin/src/main/java/com/wrpys/app/wx/service/impl/CoreServiceImpl.java
@@ -1,6 +1,8 @@
 package com.wrpys.app.wx.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.wrpys.app.wx.model.resp.Article;
+import com.wrpys.app.wx.model.resp.NewsMessage;
 import com.wrpys.app.wx.model.resp.TextMessage;
 import com.wrpys.app.wx.service.CoreService;
 import com.wrpys.app.wx.utils.MessageUtil;
@@ -9,7 +11,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -49,8 +53,62 @@ public class CoreServiceImpl implements CoreService {
 
             // 文本消息
             if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_TEXT)) {
+
                 String content = requestMap.get("Content");
-                respContent = "您发送的是文本消息！" + content;
+                if ("图片".equals(content)) {
+                    NewsMessage newsMessage = new NewsMessage();
+                    newsMessage.setMsgType(MessageUtil.RESP_MESSAGE_TYPE_NEWS);
+                    newsMessage.setToUserName(fromUserName);
+                    newsMessage.setFromUserName(toUserName);
+                    newsMessage.setCreateTime(new Date().getTime());
+                    newsMessage.setFuncFlag(0);
+
+                    List<Article> articles = new ArrayList<>();
+                    Article article = new Article();
+                    article.setTitle("标题");
+                    article.setPicUrl("https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png");
+                    article.setUrl("https://www.baidu.com/");
+                    article.setDescription("测试描述");
+                    articles.add(article);
+
+                    article = new Article();
+                    article.setTitle("大家的事由大家商量着办——习近平主席致第四届世界互联网大会的贺信引起与会中外嘉宾热烈反响");
+//                    article.setPicUrl("https://mp.weixin.qq.com/misc/getqrcode?fakeid=3074025666&token=205380857&style=1");
+                    article.setUrl("https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_15464600260927191937%22%7D&n_type=0&p_from=1");
+                    article.setDescription("大家的事由大家商量着办——习近平主席致第四届世界互联网大会的贺信引起与会中外嘉宾热烈反响");
+                    articles.add(article);
+
+                    article = new Article();
+                    article.setTitle("某网友吃冰激凌吃出一条真蛇，他真的很无耻！");
+                    article.setPicUrl("https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=1192839229,3053310621&fm=173&s=DDFE34D7560046E204B6A9740300D06B&w=640&h=397&img.JPEG");
+                    article.setUrl("https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_9860825529527486302%22%7D&n_type=0&p_from=1");
+                    article.setDescription("某网友吃冰激凌吃出一条真蛇，他真的很无耻！");
+                    articles.add(article);
+
+                    String baseUrl = "http://5aa06d97.ngrok.io";
+                    article = new Article();
+                    article.setTitle("wrpys");
+                    article.setUrl(baseUrl + "/index");
+                    article.setPicUrl(baseUrl + "/common/images/erweima.jpg");
+                    article.setDescription("wrpys微信公众号");
+                    articles.add(article);
+
+                    article = new Article();
+                    article.setTitle("wrpys头像");
+                    article.setUrl(baseUrl + "/index");
+                    article.setPicUrl(baseUrl + "/common/images/wrpys_icon.jpg");
+                    article.setDescription("wrpys微信公众号头像");
+                    articles.add(article);
+
+                    newsMessage.setArticles(articles);
+                    newsMessage.setArticleCount(articles.size());
+                    return MessageUtil.newsMessageToXml(newsMessage);
+                } else if ("文本链接".equals(content)) {
+                    respContent = "<a href=\"http://blog.csdn.net/lyq8479\">wrpys主页</a>";
+                } else {
+                    respContent = "您发送的是文本消息！" + content;
+                }
+
             }
             // 图片消息
             else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_IMAGE)) {
