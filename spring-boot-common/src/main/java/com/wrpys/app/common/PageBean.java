86fe735963c3ad7a014cca822683a93d6c0e15fc
wrpys <496732709@qq.com>
2017-12-06 14:34:56 +0800

    spring boot common

--- spring-boot-common/src/main/java/com/wrpys/app/common/PageBean.java
+++ spring-boot-common/src/main/java/com/wrpys/app/common/PageBean.java
@@ -5,34 +5,54 @@ import java.util.List;
 /**
  * @author wrp
  * @Description 分页查询对象
- * @Date 2017/10/18
+ * @Date 2017/12/6
  */
 public class PageBean {
 
-    private Long total;
-    private List<?> rows;
+    private String code;
+    private String msg;
+    private Long count;
+    private List<?> data;
 
     public PageBean() {
     }
 
-    public PageBean(Long total, List<?> rows) {
-        this.total = total;
-        this.rows = rows;
+    public PageBean(String code, String msg, Long count, List<?> data) {
+        this.code = code;
+        this.msg = msg;
+        this.count = count;
+        this.data = data;
     }
 
-    public Long getTotal() {
-        return total;
+    public String getCode() {
+        return code;
     }
 
-    public void setTotal(Long total) {
-        this.total = total;
+    public void setCode(String code) {
+        this.code = code;
     }
 
-    public List<?> getRows() {
-        return rows;
+    public String getMsg() {
+        return msg;
     }
 
-    public void setRows(List<?> rows) {
-        this.rows = rows;
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    public List<?> getData() {
+        return data;
+    }
+
+    public void setData(List<?> data) {
+        this.data = data;
     }
 }
